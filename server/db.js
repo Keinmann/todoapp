2a42866290fad19e41860a144c07d84e6867a746
keinmann <keinmann@mail.ru>
2023-08-21 23:35:44 +0300

    finalized db

--- server/db.js
+++ server/db.js
@@ -2,16 +2,11 @@ const Pool = require('pg').Pool;
 require('dotenv').config()
 
 const pool = new Pool({
-    // user: "postgres",
-    // password: "postgres",
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // database: process.env.DB_DATABASE
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'todoapp'
+    user: "postgres",
+    password: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 });
 
 
