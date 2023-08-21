6dd2f7486cff2cf69f12c20c2f6d315fac023747
keinmann <keinmann@mail.ru>
2023-08-21 23:34:33 +0300

    auth now really works.

--- server/db.js
+++ server/db.js
@@ -2,11 +2,16 @@ const Pool = require('pg').Pool;
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    // user: "postgres",
+    // password: "postgres",
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_DATABASE
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'todoapp'
 });
 
 
--- server/server.js
+++ server/server.js
@@ -16,40 +16,20 @@ app.use(express.json());
 //     res.send("SERVER MAIN");
 // });
 
-//register
-app.post('auth/signup', async (req, res) => {
-    const { email, password } = req.body;
-    console.log("register", email, password);
-    try {
-        const signUp = await db.query('INSERT INTO users(email, hashed_password) VALUES($1, $2)', [email, hashedPassword]);
-        console.log(signUp);
-    } catch (error) {
-        console.log(error);
-    }
-});
 
-//login
-app.post('auth/signin', async (req, res) => {
-    const { email, password } = req.body;
-    console.log("login", email, password);
-    try {
-        const signIn = await db.query('SELECT hashed_password FROM users WHERE email = $1', [email]);
-        console.log(signIn.rows[0]);
-        const comparison = await bcrypt.compareSync(password, signIn.rows[0].hashed_password);
-        console.log("comparison", comparison);
-    } catch (error) {
-        console.log(error);
-    }
-});
-
+//login & register
 app.post('/auth', async (req, res) => {
     const { endpoint, email, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
+    console.log(endpoint);
     if (endpoint === 'signup') {
+
         try {
             const signUp = await db.query('INSERT INTO users(email, hashed_password) VALUES($1, $2)', [email, hashedPassword]);
             console.log(signUp);
+            const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' });
+            res.json({ email, token });
         } catch (error) {
             console.log(error);
             res.json({ 'detail': 'error' });
